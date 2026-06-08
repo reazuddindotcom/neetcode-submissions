dcdd0ebd2e21ddffdf118817633876a8697f2b56
reazuddindotcom <82423658+reazuddindotcom@users.noreply.github.com>
2026-06-07 17:26:30 -0700

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,38 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # create adj list
+        # use a searched (visited) map
+        # for each un-visited course start dfs
+        # for each dfs search use a separated visited tracking
+        adj_list = {i: [] for i in range(numCourses)} # defaultdict(list)
+        for req in prerequisites:
+            adj_list[req[0]].append(req[1]) # no duplicate input
+
+        visited = [False]*numCourses
+        
+        for course in adj_list:
+            if not visited[course]:
+                prereq = [False]*numCourses
+                if not self.dfs(course, adj_list, visited, prereq):
+                    return False
+
+        return True
+
+    def dfs(self, course: int, adj_list: map, visited: [], prereq: []) -> bool:
+        if prereq[course]:
+            return False
+        # If we visited - that means we searched this path and 
+        # didn't return False. So we can just return True.
+        if visited[course]:
+            return True
+
+        visited[course] = True # what if we mark visited here??
+        prereq[course] = True
+        for c in adj_list[course]:
+            if not self.dfs(c, adj_list, visited, prereq):
+                return False
+        prereq[course] = False
+
+        return True
+
+        
